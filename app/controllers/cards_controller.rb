7ed7bbe4dba8da69a17f6a624bec6bd6c6223492
Tia-Koenig <tiakoenig1994@gmail.com>
2021-11-14 12:46:51 +1000

    added devise login/signup, added styling, created global stylings, changed/added styling

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,6 +1,25 @@
 class CardsController < ApplicationController
+    before_action :authenticate_user!, except: [:index]
+    #add :show when created ^ 
+
 
     def index
+        # @cards = Card.order(number: :asc)
+    end
+
+    def new 
+    end
+
+    def create 
+    end 
+
+    def update 
+    end
+
+    def delete 
+    end
+
+    def show 
     end
     
 end
