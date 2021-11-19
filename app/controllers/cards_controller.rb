1f5971afc9d6a87b91cb1e7b81d0aca5e32aaff2
Tia-Koenig <tiakoenig1994@gmail.com>
2021-11-19 22:39:13 +1000

    finished edit/update

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,32 +1,34 @@
 class CardsController < ApplicationController
+    before_action :setup_genset_and_card_condition, only: [:new, :edit]
+    before_action :find_card_setup, only: [:update, :edit, :show]
     # before_action :authenticate_user!, except: [:index]
     #add :show when created ^ 
 
 
     def index
         @cards = Card.order(title: :asc)
+        @gensets = Genset.all
     end
     
     def new 
         @card = Card.new 
-        @genset = Genset.order(:id)
-        @card_condition = CardCondition.order(:id)
     end
 
     def create 
         @card = Card.new(card_params)
         @card.user_id = current_user.id
         @card.save
-
         redirect_to card_path(@card.id)
     end 
 
     def edit 
-        
+        find_card_setup
     end
 
     def update 
-        
+        find_card_setup
+        @card.update(card_params)
+        redirect_to card_path(@card.id)
     end
 
     def destroy 
@@ -34,7 +36,7 @@ class CardsController < ApplicationController
     end
 
     def show 
-        @card = Card.find(params[:id])
+        find_card_setup
         @genset = Genset.find(@card.genset_id)
         @card_condition = CardCondition.find(@card.card_condition_id)
     end
@@ -46,5 +48,13 @@ class CardsController < ApplicationController
     def card_params
         params.require(:card).permit(:title, :number, :price, :genset_id, :card_condition_id)
     end
-    
+
+    def setup_genset_and_card_condition
+        @genset = Genset.order(:id)
+        @card_condition = CardCondition.order(:id)
+    end
+
+    def find_card_setup
+        @card = Card.find(params[:id])
+    end
 end
