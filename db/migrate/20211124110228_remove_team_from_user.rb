f32fe46e26dba59789973ac677148fd587b0e1f5
Tia-Koenig <tiakoenig1994@gmail.com>
2021-11-26 18:04:00 +1000

    trying to fix migratyion error

--- db/migrate/20211124110228_remove_team_from_user.rb
+++
@@ -1,5 +0,0 @@
-class RemoveTeamFromUser < ActiveRecord::Migration[6.1]
-  def change
-    remove_column :users, :team_id, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -79,12 +79,6 @@ ActiveRecord::Schema.define(version: 2021_11_24_110228) do
     t.index ["resource_type", "resource_id"], name: "index_roles_on_resource"
   end
 
-  create_table "teams", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
